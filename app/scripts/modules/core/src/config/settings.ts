38f1b03483b610fac5393fb1e77203741da16db8
DarrenN <info@v25media.com>
2017-12-21 12:20:35 -0500

    Updates formatting to get closed to Spinnaker's editorconfig

--- app/scripts/modules/core/src/config/settings.ts
+++ app/scripts/modules/core/src/config/settings.ts
@@ -1,88 +1,88 @@
-import { cloneDeep, merge } from 'lodash';
+import { cloneDeep, merge } from "lodash";
 
 export interface IProviderSettings {
-    defaults: any;
-    resetToOriginal?: () => void;
+  defaults: any;
+  resetToOriginal?: () => void;
 }
 
 export interface INotificationSettings {
-    email: {
-        enabled: boolean;
-    };
-    hipchat: {
-        enabled: boolean;
-        botName: string;
-    };
-    sms: {
-        enabled: boolean;
-    };
-    slack: {
-        enabled: boolean;
-        botName: string;
-    };
+  email: {
+    enabled: boolean;
+  };
+  hipchat: {
+    enabled: boolean;
+    botName: string;
+  };
+  sms: {
+    enabled: boolean;
+  };
+  slack: {
+    enabled: boolean;
+    botName: string;
+  };
 }
 
 export interface IFeatures {
-    canary?: boolean;
-    entityTags?: boolean;
-    fiatEnabled?: boolean;
-    pipelines?: boolean;
-    notifications?: boolean;
-    clusterDiff?: boolean;
-    roscoMode?: boolean;
-    chaosMonkey?: boolean;
-    // whether stages affecting infrastructure (like "Create Load Balancer") should be enabled or not
-    infrastructureStages?: boolean;
-    jobs?: boolean;
-    snapshots?: boolean;
-    dockerBake?: boolean;
-    pipelineTemplates?: boolean;
-    versionedProviders?: boolean;
-    travis?: boolean;
-    [key: string]: any;
+  canary?: boolean;
+  entityTags?: boolean;
+  fiatEnabled?: boolean;
+  pipelines?: boolean;
+  notifications?: boolean;
+  clusterDiff?: boolean;
+  roscoMode?: boolean;
+  chaosMonkey?: boolean;
+  // whether stages affecting infrastructure (like "Create Load Balancer") should be enabled or not
+  infrastructureStages?: boolean;
+  jobs?: boolean;
+  snapshots?: boolean;
+  dockerBake?: boolean;
+  pipelineTemplates?: boolean;
+  versionedProviders?: boolean;
+  travis?: boolean;
+  [key: string]: any;
 }
 
 export interface ISpinnakerSettings {
-    [key: string]: any;
+  [key: string]: any;
 
-    analytics: { ga?: string };
-    authEnabled: boolean;
-    authEndpoint: string;
-    authTtl: number;
-    bakeryDetailUrl: string;
-    changelog?: {
-        accessToken?: string;
-        fileName: string;
-        gistId: string;
+  analytics: { ga?: string };
+  authEnabled: boolean;
+  authEndpoint: string;
+  authTtl: number;
+  bakeryDetailUrl: string;
+  changelog?: {
+    accessToken?: string;
+    fileName: string;
+    gistId: string;
+  };
+  checkForUpdates: boolean;
+  debugEnabled: boolean;
+  defaultCategory: string;
+  defaultInstancePort: number;
+  defaultProviders: string[];
+  defaultTimeZone: string; // see http://momentjs.com/timezone/docs/#/data-utilities/
+  entityTags?: {
+    maxUrlLength?: number;
+  };
+  executionWindow?: {
+    atlas?: {
+      regions: { label: string; baseUrl: string }[];
+      url: string;
     };
-    checkForUpdates: boolean;
-    debugEnabled: boolean;
-    defaultCategory: string;
-    defaultInstancePort: number;
-    defaultProviders: string[];
-    defaultTimeZone: string; // see http://momentjs.com/timezone/docs/#/data-utilities/
-    entityTags?: {
-        maxUrlLength?: number;
-    };
-    executionWindow?: {
-        atlas?: {
-            regions: { label: string; baseUrl: string }[];
-            url: string;
-        };
-    };
-    feature: IFeatures;
-    gateUrl: string;
-    gitSources: string[];
-    notifications: INotificationSettings;
-    pollSchedule: number;
-    providers?: {
-        [key: string]: IProviderSettings; // allows custom providers not typed in here (good for testing too)
-    };
-    resetProvider: (provider: string) => () => void;
-    resetToOriginal: () => void;
-    searchVersion: 1 | 2;
-    securityGroupsReadOnly: boolean;
-    triggerTypes: string[];
+  };
+  feature: IFeatures;
+  gateUrl: string;
+  gitSources: string[];
+  notifications: INotificationSettings;
+  pollSchedule: number;
+  providers?: {
+    [key: string]: IProviderSettings; // allows custom providers not typed in here (good for testing too)
+  };
+  resetProvider: (provider: string) => () => void;
+  resetToOriginal: () => void;
+  searchVersion: 1 | 2;
+  securityGroupsReadOnly: boolean;
+  triggerTypes: string[];
 }
 
 export const SETTINGS: ISpinnakerSettings = (<any>window).spinnakerSettings;
@@ -91,24 +91,23 @@ export const SETTINGS: ISpinnakerSettings = (<any>window).spinnakerSettings;
 SETTINGS.feature = SETTINGS.feature || {};
 SETTINGS.analytics = SETTINGS.analytics || {};
 SETTINGS.providers = SETTINGS.providers || {};
-SETTINGS.defaultTimeZone = SETTINGS.defaultTimeZone || 'America/Los_Angeles';
+SETTINGS.defaultTimeZone = SETTINGS.defaultTimeZone || "America/Los_Angeles";
 
 // A helper to make resetting settings to steady state after running tests easier
 const originalSettings: ISpinnakerSettings = cloneDeep(SETTINGS);
 SETTINGS.resetToOriginal = () => {
-    Object.keys(SETTINGS)
-        .filter(k => typeof SETTINGS[k] !== 'function') // maybe don't self-destruct
-        .forEach(k => delete SETTINGS[k]);
-    merge(SETTINGS, originalSettings);
+  Object.keys(SETTINGS)
+    .filter(k => typeof SETTINGS[k] !== "function") // maybe don't self-destruct
+    .forEach(k => delete SETTINGS[k]);
+  merge(SETTINGS, originalSettings);
 };
 
 SETTINGS.resetProvider = (provider: string) => {
-    return () => {
-        const providerSettings: IProviderSettings =
-            SETTINGS.providers[provider];
-        Object.keys(providerSettings)
-            .filter(k => typeof (providerSettings as any)[k] !== 'function')
-            .forEach(k => delete (providerSettings as any)[k]);
-        merge(providerSettings, originalSettings.providers[provider]);
-    };
+  return () => {
+    const providerSettings: IProviderSettings = SETTINGS.providers[provider];
+    Object.keys(providerSettings)
+      .filter(k => typeof (providerSettings as any)[k] !== "function")
+      .forEach(k => delete (providerSettings as any)[k]);
+    merge(providerSettings, originalSettings.providers[provider]);
+  };
 };
